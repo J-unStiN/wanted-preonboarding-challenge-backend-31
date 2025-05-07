83ce2de116489a850bb69ed1c395d895bc6a2f4b
Park Sung Jun <junstin119@gmail.com>
2025-05-08 02:34:54 +0900

    feat - 공통 API 생성.

---
+++ backend/src/main/java/com/ex/backend/common/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.ex.backend.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private boolean success;
+    private T data;
+    private String message;
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(true, data, message);
+    }
+
+    public static <T> ApiResponse<T> error(T error, String message) {
+        return new ApiResponse<>(false, error, message);
+    }
+
+}
